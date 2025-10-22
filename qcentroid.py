c64eaa455384a072a9d25573ddfca71085fe4bec
Oscar Bastidas Jossa <oscarjavier.jb@gmail.com>
2025-10-22 10:38:31 +0200

    Added needed qcentroid files

---
+++ qcentroid.py
@@ -0,0 +1,19 @@
+#
+# QCentroid entrypoint
+# 
+# DO NOT change the name of this file.
+# DO NOT edit the signature of the run() function.
+#
+# Import here any non-standard packages you need or your custom modules
+#
+
+def run(input_data:dict, solver_params:dict, extra_arguments:dict) -> dict:
+
+    #
+    # Add your solver's code here, or call it from here if it is already implemented in another module
+    #
+
+    output = {"message": "Hello world!"}
+
+    # And this is the output it returns. It must be a dictionary.
+    return output
